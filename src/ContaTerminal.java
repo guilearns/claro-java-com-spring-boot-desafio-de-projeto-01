3a4668e76a5b94dbc05705d391c3a97f4192ad6b
guilearns <guilearns@outlook.com>
2024-08-18 22:34:47 -0300

    implementação inicial do desafio de sintaxe

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,12 +1,43 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaTerminal {
 	public static void main(String[] args) throws Exception {
-		//TODO: Conhecer e importar a classe Scanner
 		
-		//Exibir as mensagens para o nosso usuário
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 		
-		//Obter pela scanner os valores digitados no terminal
+		System.out.println("Agradecemos a sua escolha para criar uma conta conosco!\n");
 		
-		//Exibir a mensagem conta criada
+		String nomeCliente = receberEntradaTexto("Por favor, digite o seu nome e sobrenome: ", scanner);
+		
+		String numeroConta = receberEntradaTexto("Por favor, digite o número da Agência: ", scanner);
+		
+		int numeroAgencia = receberEntradaInteiro("Por favor, digite o número da Conta: ", scanner);
+		
+		float saldoConta = receberEntradaDecimal("Por favor, faça depósito inicial: R$ ", scanner);
+
+		System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %s e seu saldo de R$%.2f já está disponível para saque.", nomeCliente, numeroAgencia, numeroConta, saldoConta);
 	}
+	
+	private static String receberEntradaTexto(String mensagemPergunta, Scanner scanner) {
+		System.out.print(mensagemPergunta);
+		String saidaTexto = scanner.next();
+		System.out.println();
+		return saidaTexto;
+	}
+	
+	private static int receberEntradaInteiro(String mensagemPergunta, Scanner scanner) {
+		System.out.print(mensagemPergunta);
+		int saidaInteiro = scanner.nextInt();
+		System.out.println();
+		return saidaInteiro;
+	}
+	
+	private static float receberEntradaDecimal(String mensagemPergunta, Scanner scanner) {
+		System.out.print(mensagemPergunta);
+		float saidaInteiro = scanner.nextFloat();
+		System.out.println();
+		return saidaInteiro;
+	}
+	
 }
